7f70189a889f7b39176ec90747026ccd767dd8e1
OndrejValla <ondrej.valla@gmail.com>
2021-06-03 18:27:57 +0000

    Landing page destinations cards, FLIP cards on click JS function created

--- assets/js/my-script.js
+++ assets/js/my-script.js
@@ -4,3 +4,13 @@ $(document).ready(function () {
     $("h2").removeClass("underline");
     $("ul").addClass("border"); //this class needs to exist in css file
 });
+
+
+// Flip cards function
+const card = document.querySelectorAll('.card-body');
+
+card.forEach((card) => {
+    card.addEventListener("click", () => {
+        card.classList.toggle("is-flipped");
+    });
+});
