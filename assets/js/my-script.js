af0899df3808fc2eb93c9a1c90c2008a4e817161
OndrejValla <ondrej.valla@gmail.com>
2021-06-10 14:21:54 +0000

    Destinations A B C D E F marked on the map by using  Google Maps API

--- assets/js/my-script.js
+++ assets/js/my-script.js
@@ -4,8 +4,6 @@ function bodyLoaded() {
 
 
 // Zuzana Ondrej Logo
-
-
 $(".fa-heart").mouseenter(function () {
     $(".fa-heart").css("color", "red")
 });
@@ -33,9 +31,19 @@ $("#ondrej-button").on("click", function () {
     $(".intro-p").addClass('intro-p-blue');
     $("#intro-text-box").removeClass('intro-text-pink').addClass('intro-text-blue');
     $(".gallery-window-row").removeClass('gallery-window-row-pink').addClass('gallery-window-row-blue');
+    $(".gallery-message-h3").css("color", "whitesmoke");
 });
 
 // Gallery page destinations buttons
+//$(".gallery-window-row").ready(function () {
+//$(".dubai-window").hide('fast');
+//$(".cuba-window").hide('fast');
+//$(".singapore-window").hide('fast');
+//$(".java-window").hide('fast');
+// $(".miami-window").hide('fast');
+//$(".disney-window").hide('fast');
+//});
+
 $("#dubai-button").on("click", function () {
     $(".dubai-window").show('medium');
     $(".cuba-window").hide('fast');
@@ -43,6 +51,7 @@ $("#dubai-button").on("click", function () {
     $(".java-window").hide('fast');
     $(".miami-window").hide('fast');
     $(".disney-window").hide('fast');
+    $(".gallery-message-h3").hide('fast');
 });
 
 $("#cuba-button").on("click", function () {
@@ -52,6 +61,7 @@ $("#cuba-button").on("click", function () {
     $(".java-window").hide('fast');
     $(".miami-window").hide('fast');
     $(".disney-window").hide('fast');
+    $(".gallery-message-h3").hide('fast');
 });
 
 $("#singapore-button").on("click", function () {
@@ -61,6 +71,7 @@ $("#singapore-button").on("click", function () {
     $(".java-window").hide('fast');
     $(".miami-window").hide('fast');
     $(".disney-window").hide('fast');
+    $(".gallery-message-h3").hide('fast');
 });
 
 $("#java-button").on("click", function () {
@@ -70,6 +81,7 @@ $("#java-button").on("click", function () {
     $(".singapore-window").hide('fast');
     $(".miami-window").hide('fast');
     $(".disney-window").hide('fast');
+    $(".gallery-message-h3").hide('fast');
 });
 
 $("#miami-button").on("click", function () {
@@ -79,6 +91,7 @@ $("#miami-button").on("click", function () {
     $(".singapore-window").hide('fast');
     $(".java-window").hide('fast');
     $(".disney-window").hide('fast');
+    $(".gallery-message-h3").hide('fast');
 });
 
 $("#disney-button").on("click", function () {
@@ -88,6 +101,7 @@ $("#disney-button").on("click", function () {
     $(".singapore-window").hide('fast');
     $(".java-window").hide('fast');
     $(".miami-window").hide('fast');
+    $(".gallery-message-h3").hide('fast');
 });
 
 
@@ -95,7 +109,7 @@ $("#disney-button").on("click", function () {
 
 
 
-// Flip cards function
+// Flip cards function Inspired by Youtube and Web search
 const card = document.querySelectorAll('.card-body');
 
 card.forEach((card) => {
@@ -103,3 +117,53 @@ card.forEach((card) => {
         card.classList.toggle("is-flipped");
     });
 });
+
+// Google Maps API followed instructions of Google Maps Tutorials
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map-window"), {
+        zoom: 1,
+        center: {
+            lat: 27.463730054771254,
+            lng: 20.3533478093073
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var markers = locations.map((location, i) => {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    new MarkerClusterer(map, markers, {
+        imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
+}
+
+var locations = [{
+        lat: 25.19367032562668,
+        lng: 55.27234906051312
+    },
+    {
+        lat: 23.188698773504868,
+        lng: -81.17732827834554
+    },
+    {
+        lat: 1.303255854524248,
+        lng: 103.83279923972277
+    },
+    {
+        lat: -7.819180600764947,
+        lng: 110.37204796688415
+    },
+    {
+        lat: 25.778265146658438,
+        lng: -80.21997333565376
+    },
+    {
+        lat: 48.86873130712801,
+        lng: 2.781827944463382
+    }
+];
