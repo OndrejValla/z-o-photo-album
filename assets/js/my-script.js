6cdc31e594abc27030aa1b021d0c83a3be153af5
OndrejValla <ondrej.valla@gmail.com>
2021-06-15 21:44:44 +0000

    Created test.html to practice JS  Random img function

--- assets/js/my-script.js
+++ assets/js/my-script.js
@@ -170,3 +170,62 @@ var locations = [{
         lng: 2.781827944463382
     }
 ];
+
+
+
+
+
+
+
+
+// T E S T
+
+// https://www.w3resource.com/javascript-exercises/javascript-dom-exercise-11.php
+
+function display_random_image() {
+    var theImages = [{
+        src: "assets/images/dubai/dubai-1.JPG",
+        width: "356",
+        height: "536"
+    }, {
+        src: "assets/images/dubai/dubai-12.JPG",
+        width: "832",
+        height: "554"
+    }, {
+        src: "assets/images/dubai/dubai-2.JPG",
+        width: "832",
+        height: "555"
+    }];
+
+    var preBuffer = [];
+    for (var i = 0, j = theImages.length; i < j; i++) {
+        preBuffer[i] = new Image();
+        preBuffer[i].src = theImages[i].src;
+        preBuffer[i].width = theImages[i].width;
+        preBuffer[i].height = theImages[i].height;
+    }
+
+    // create random image number
+    function getRandomInt(min, max) {
+        //  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+        imn = Math.floor(Math.random() * (max - min + 1)) + min;
+        return preBuffer[imn];
+    }
+
+    // 0 is first image,   preBuffer.length - 1) is  last image
+
+    var newImage = getRandomInt(0, preBuffer.length - 1);
+
+    // remove the previous images
+    var images = document.getElementsByTagName('img');
+    var l = images.length;
+    for (var p = 0; p < l; p++) {
+        images[0].parentNode.removeChild(images[0]);
+    }
+    // display the image  
+    document.getElementById("randomdiv").appendChild(newImage);
+}
+
+//document.getElementById("randomdiv").appendChild(newImage);
+//document.body.appendChild(newImage);
