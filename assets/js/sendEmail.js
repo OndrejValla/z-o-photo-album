f3fd2f58752455b3f842c01317ff94cec00b84a0
OndrejValla <ondrej.valla@gmail.com>
2021-06-17 18:45:44 +0000

    Greeting 'Have a great day/evening' based on the time of the day added to contact.html

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -35,3 +35,20 @@ $(document).ready(function () {
         }, 3000);
     });
 });
+
+// Greeting based on the time of the day.
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_elseif
+function ourGreeting() {
+    var comunication;
+    var time = new Date().getHours();
+    if (time < 12) {
+        comunication = "Have a great day!";
+    } else if (time < 18) {
+        comunication = "Have a great afternoon!";
+    } else {
+        comunication = "Have a lovely evening!";
+    }
+    document.getElementById("interaction-h3").innerHTML = "Thank You for your message.";
+    document.getElementById("interaction-h4").innerHTML = comunication;
+    document.getElementById("interaction-p").innerHTML = "<br> Kind Regards <br> Zuzana and Ondrej";
+}
