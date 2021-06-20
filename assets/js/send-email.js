67dda94b792e8b1c27b655d6e5321104453768e8
OndrejValla <ondrej.valla@gmail.com>
2021-06-20 14:27:44 +0000

    Failed and Success message added to contact form

--- assets/js/send-email.js
+++ assets/js/send-email.js
@@ -1,6 +1,5 @@
 // Contact form
 // Inspired by Mr Matt Rudge
-
 console.log("Contact Form Loaded");
 
 function sendMail(contactForm) {
@@ -12,28 +11,26 @@ function sendMail(contactForm) {
         .then(
             function (response) {
                 console.log("SUCCESS", response);
+                ourGreeting();
+                alert("Your message has been sent! Thank You");
             },
+
             function (error) {
                 console.log("FAILED", error);
+                failedMessage();
+                alert("ERROR Your message has NOT been sent!");
             }
         );
     return false;
-}
+};
 
 // https://stackoverflow.com/questions/37525339/change-submit-button-text-after-form-submit
-$(document).ready(function () {
-    $("#submit-button").click(function (event) {
-        $('#submit-button').val('Sending message...');
-    });
-    $('form').submit(function (event) {
-        setTimeout(function () {
-            $('#submit-button').val('Send');
-            $('#contact-form')[0].reset();
-        }, 2000);
-        setTimeout(function () {
-            alert("Your message has been sent! Thank You");
-        }, 3000);
-    });
+$("#submit-button").click(function (event) {
+    $('#submit-button').val('Sending message...');
+    setTimeout(function () {
+        $('#submit-button').val('Send');
+        $('#contact-form')[0].reset();
+    }, 2000);
 });
 
 // Greeting based on the time of the day.
@@ -45,10 +42,17 @@ function ourGreeting() {
         comunication = "Have a great day!";
     } else if (time < 18) {
         comunication = "Have a great afternoon!";
+
     } else {
         comunication = "Have a lovely evening!";
     }
     document.getElementById("interaction-h3").innerHTML = "Thank You for your message.";
     document.getElementById("interaction-h4").innerHTML = comunication;
     document.getElementById("interaction-p").innerHTML = "<br> Kind Regards <br> Zuzana and Ondrej";
+}
+
+// Failed message function
+function failedMessage() {
+    document.getElementById("interaction-h3").innerHTML = "There was a problem.<br>Your message was not send.";
+    document.getElementById("interaction-p").innerHTML = "Please try again, or contact us directly on <br> ondrej.valla@gmail.com <br> Thank you.";
 }
