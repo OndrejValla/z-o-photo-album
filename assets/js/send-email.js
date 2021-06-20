ed17484383a86239b0bcc5c90f966c7e11844f51
OndrejValla <ondrej.valla@gmail.com>
2021-06-20 16:47:10 +0000

    Key Events keydown keypress  keyup  functions added to the contact.html.

--- assets/js/send-email.js
+++ assets/js/send-email.js
@@ -56,3 +56,26 @@ function failedMessage() {
     document.getElementById("interaction-h3").innerHTML = "There was a problem.<br>Your message was not send.";
     document.getElementById("interaction-p").innerHTML = "Please try again, or contact us directly on <br> ondrej.valla@gmail.com <br> Thank you.";
 }
+
+// Smiling icons color change
+// Inspired by Code Institute 
+// JavaScript & the DOM  Handling DOM Events  Keyboard Events
+function pressedKeys(event) {
+    let pressedBeam = document.getElementById('pressed-beam');
+    let upSmile = document.getElementById('up-smile');
+    let downBeam = document.getElementById('down-beam');
+
+    if (event.repeat) {
+        return false;
+    } else {
+        if (event.type === 'keydown' || event.type === 'keypress') {
+            downBeam.style.color = 'rgb(165, 201, 236)';
+            pressedBeam.style.color = 'rgb(250, 209, 209)';
+            upSmile.style.color = 'rgb(230, 52, 52)';
+        } else if (event.type === 'keyup') {
+            downBeam.style.color = 'rgb(23, 75, 126)';
+            pressedBeam.style.color = 'rgb(201, 101, 101)';
+            upSmile.style.color = 'rgba(245, 205, 207, 0.552)';
+        }
+    }
+};
